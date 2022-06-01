5b908b16fb32232341a1faa7fff37432bca98731
naokiuma <sagalumaata@gmail.com>
2022-06-02 01:11:54 +0900

    form少しいじり

--- src/components/TextInput.tsx
+++ src/components/TextInput.tsx
@@ -1,17 +1,18 @@
-import React, { useRef } from 'react';
+import React, { useRef,useState } from 'react';
 
 export const TextInput = () => {
     //const myRef = React.createRef();//createrefはクラスコンポーネントで使う
     const inputElement = useRef(null);//userefは関数コンポーネントで使える
-    const handleClick = (): void => {
-        inputElement.current.focus();
-        console.log("focusする。" + inputElement.current)
-    };
+    const [name, setName] = useState("あいう");
+   
 
     return (
-    <div>
-        <input type="text" ref={inputElement} />
-        <input type="button" value="Focus" onClick={handleClick}/>
-    </div>
+        <form action="">
+            <label htmlFor="">
+                <input type="text" value={name} name="name"/>
+            </label>
+            <input type="submit" value="submit"/>
+
+        </form>
     ); 
 };
