081ba94b16d5f3c1e9233d4a1734b3a981f16e62
naokiuma <sagalumaata@gmail.com>
2022-07-08 01:24:59 +0900

    css_inlinestyle 使いづらい

--- src/components/global/Topic.tsx
+++ src/components/global/Topic.tsx
@@ -3,20 +3,22 @@ import {TopicType} from "../../types/topicType";
 
 export const Topic = (props:TopicType) => {
     const { title,id,status } = props;
+
+    const topic_card ={
+        width:"30%"
+    }
+
     return (
-        <>
-           <ul>
-               <li>
-                   <span>
-                       タイトル：{title}
-                    </span>
-                    <br/>
-                   <span>
-                       ステータス；{status}
-                    </span>
-               </li>
-           </ul>
-        </>
+        <div style={topic_card}>
+            <span>
+                タイトル：{title}
+            </span>
+            <br/>
+            <span>
+                ステータス；{status}
+            </span>
+        </div>
+        
     )
 }
   
--- src/components/pages/Home.tsx
+++ src/components/pages/Home.tsx
@@ -3,15 +3,31 @@ import { memo,FC } from "react";
 import { Testfetch } from "../../fetch/Testfetch";
 import { Topic } from "../global/Topic";
 
-
+// export const InlineStyle = () => {
+//     const Container ={
+//       border:"1px solid black",
+//       borderRadius:"20px"
+  
+//     }
+//     return (
+//       <div style={Container}>
+//         <p>-inline styles -</p>
+//         <button>ボタン</button>
+//       </div>
+//     );
+//   };
 
 export const Home:FC = memo(() => {
+
+    const Topics ={
+        display:"flex",
+    }
     return (
         <section className="home main_contents">
             <p>
                 ホームです。
             </p>
-            <div className="topics">
+            <div style={Topics}>
                 <Topic title={'やあ'} id={1} status={'話し中'}/>
                 <Topic title={'ダイイングライトをプレイ中'} id={1} status={'話し中'}/>
                 <Topic title={'ゼルダ面白かったー！'} id={1} status={'完了'}/>
